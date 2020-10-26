ae0e832eecce094672d74b3e47852470637dd769
Rolanda <46946118+rolandadifan@users.noreply.github.com>
2020-10-26 16:13:02 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const nodemailer = require('nodemailer')
+
+const app = express()
+
+var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: 'email gmail',
+        pass: 'password gmail'
+    } 
+})
+
+var mailOptions = {
+    from: 'zirex28@gmail.com',
+    to: 'rolandadifandana@gmail.com',
+    subject: 'sending email using node.js',
+    text: 'ini hanya percobaan',
+    html: '<h1>Verification</h1><p>verify email this link</p> <a href="https://www.facebook.com/">here</a>'
+}
+
+transporter.sendMail(mailOptions,function(error, info){
+    if(error){
+        console.log(error)
+    }else{
+        console.log('Email sent: ' + info.response)
+    }
+})
+
+app.get('/',(req,res)=>{
+    res.send('<h1>welcome</h1>')
+})
+
+app.listen(3000)
